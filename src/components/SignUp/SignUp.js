43fa557f729b66cfcdd725a01ae0eb7722d08cda
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-18 14:55:59 +0600

    sign up component created and signUp and Login button toogle on both page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Sign Up</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="Email">Email</label>
+            <input type="email" name="Email" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Password">Password</label>
+            <input type="password" name="Password" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Confirm Password">Confirm Password</label>
+            <input type="password" name="Confirm Password" id="" required />
+          </div>
+          <div className="input-group">
+            <input className="form-submit" type="button" value="Sign Up" />
+          </div>
+        </form>
+        <div className="signup-in-login">
+          <p className="login-body">
+            Already have an account?{" "}
+            <Link to="/login" className="new-account">
+              Login
+            </Link>
+          </p>
+        </div>
+        <div className="login-divider">
+          <div></div>
+          <p>Or</p>
+          <div></div>
+        </div>
+        <div className="login-with-google">
+          <img src="https://img.icons8.com/color/452/google-logo.png" alt="" />
+          <p>Continue with Google</p>
+        </div>
+      </div>
     </div>
   );
 };
