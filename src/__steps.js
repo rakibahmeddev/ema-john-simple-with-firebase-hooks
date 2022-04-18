1aad1d0732100bcadda8ab8119da7dc36430c6c4
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-18 14:40:34 +0600

    login component design done

--- src/__steps.js
+++ src/__steps.js
@@ -4,7 +4,9 @@
 3. create project on firebase
 4. Register App with Hosting
 5. install firebase
-6. 
+6. create firebase.init.js and initialize ans also export getAuth
+7. Authentication-> enable sign in with Email and Password
+8. create github repo and push code
 
 
 */
---
+++ src/components/Login/Login.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="Email">Email</label>
+            <input type="email" name="Email" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Password">Password</label>
+            <input type="password" name="Password" id="" required />
+          </div>
+          <div className="input-group">
+            <input className="form-submit" type="button" value="Login" />
+          </div>
+        </form>
+        <div className="signup-in-login">
+          <p className="login-body">
+            New to Ema John?{" "}
+            <Link to="/signup" className="new-account">
+              Create New Account
+            </Link>
+          </p>
+        </div>
+        <div className="login-divider">
+          <div></div>
+          <p>Or</p>
+          <div></div>
+        </div>
+        <div className="login-with-google">
+          <img src="https://img.icons8.com/color/452/google-logo.png" alt="" />
+          <p>Continue with Google</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
